21ff1c766be3dcd68554ec8162465b4f64a83127
Barbara Becker <bbeckerdarosa@gmail.com>
2018-10-14 02:50:52 -0300

    altered type of schema mongo

--- lib/models/entityModel.ts
+++ lib/models/entityModel.ts
@@ -2,6 +2,11 @@ import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const campoDinamico = new Schema({
+    label: String,
+    valor: Schema.Types.Mixed
+});
+
 export const EntitySchema = new Schema({
     nome: String,
     localizacao: {
@@ -10,13 +15,8 @@ export const EntitySchema = new Schema({
     },
     descricao: String,
     tipo: String,
-    camposDinamicos: [
-        {
-            label: String,
-            valor: any
-        }, {}, {}
-    ],
-    keywords: [ 'pago' ],
+    camposDinamicos: [ campoDinamico ],
+    keywords: [ String ],
     created_date: {
         type: Date,
         default: Date.now
